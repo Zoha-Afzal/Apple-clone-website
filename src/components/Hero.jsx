9377d6166aea2a2947edc00c7e86404711fc15d9
Zoha Afzal <160478899+Zoha-Afzal@users.noreply.github.com>
2025-03-22 15:16:07 +0500

    Start hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,26 @@
-
-
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, samllHeroVideo} from "../utils";
 const Hero = () => {
+
+const [videoSrc, setVideoSrc] = useState(window.innerWidth <760 ? samllHeroVideo : heroVideo)
+
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay:1.5 })
+  }, [])
   return (
-    <div>
-      Hero
-    </div>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+          <p id="hero" className="hero-title">iPhone 15 Pro</p>
+
+          <div className="md:w-10/12 w-9/12">
+          <video>
+            <source src={videoSrc}/>
+          </video>
+
+          </div>
+      </div>
+    </section>
   )
 }
 
