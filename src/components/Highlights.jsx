d89fd2ff84e1ba4f3b9abc47ce9296d09e25a51f
Zoha Afzal <160478899+Zoha-Afzal@users.noreply.github.com>
2025-03-23 20:57:48 +0500

    VideoCarousel Complete

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,13 +2,14 @@ import { useEffect } from "react"
 import gsap from "gsap"
 import { rightImg, watchImg } from "../utils"
 import VideoCarousel from "./VideoCarousel"; // Ensure this path is correct and the component exists
+import { useGSAP } from "@gsap/react";
 
 
 const Highlights = () => {
 
-  useEffect(() => {
+  useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0,  });
-    gsap.to('.Link', { opacity: 1, y: 0, duration:1, stagger:0.3 });
+    gsap.to('.link', { opacity: 1, y: 0, duration:1, stagger:0.25 });
   }, []);
   return (
     <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
@@ -18,11 +19,11 @@ const Highlights = () => {
             Get the highlights
           </h1>
           <div className="flex flex-wrap items-end gap-5">
-            <p className="Link flex items-center">
+            <p className="link flex items-center">
               Watch the film
               <img src={watchImg} alt="watch" className=" ml-2" />
             </p>
-            <p className="Link flex items-center">
+            <p className="link flex items-center">
               Watch the event
               <img src={rightImg} alt="right" className=" ml-2" />
             </p>
